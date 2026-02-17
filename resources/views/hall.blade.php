1fbb9a73053d8c7b5921c4cd667f63bb470dda29
Zhafran <zisbest2010@gmail.com>
2026-02-17 11:04:40 +0700

    first commit

---
+++ resources/views/hall.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="text-5xl font-bold text-red-400">Hall</h1>
+@endsection
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <p>Ini halaman {{ $title }}</p>
+@endsection
